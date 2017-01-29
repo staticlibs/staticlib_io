f2e859ac75e0a8d8f31c8459026bac88ac37db31
alex <alex@staticlibs.net>
2017-01-29 16:59:04 +0000

    limited_source added

---
+++ include/staticlib/io/limited_source.hpp
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2017, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * File:   limited_source.hpp
+ * Author: alex
+ *
+ * Created on January 29, 2017, 1:33 PM
+ */
+
+#ifndef STATICLIB_IO_LIMITED_SOURCE_HPP
+#define	STATICLIB_IO_LIMITED_SOURCE_HPP
+
+#include <ios>
+
+#include "staticlib/config.hpp"
+
+#include "staticlib/io/IOException.hpp"
+#include "staticlib/io/counting_source.hpp"
+#include "staticlib/io/reference_source.hpp"
+
+namespace staticlib {
+namespace io {
+
+/**
+ * Source wrapper that limits the number of bytes read through it
+ * and throws "IOException" on threshold exceed
+ */
+template<typename Source>
+class limited_source {
+    /**
+     * Input source
+     */
+    staticlib::io::counting_source<Source> src;
+    
+    /**
+     * Limit in bytes
+     */
+    size_t limit_bytes;
+
+public:
+
+    /**
+     * Constructor,
+     * created source wrapper will own specified source
+     * 
+     * @param src input source
+     * @param limit_bytes max number of bytes allowed to be read from this source
+     */
+    limited_source(Source&& src, size_t limit_bytes) :
+    src(staticlib::io::make_counting_source(std::move(src))),
+    limit_bytes(limit_bytes) { }
+
+    /**
+     * Deleted copy constructor
+     * 
+     * @param other instance
+     */
+    limited_source(const limited_source&) = delete;
+
+    /**
+     * Deleted copy assignment operator
+     * 
+     * @param other instance
+     * @return this instance 
+     */
+    limited_source& operator=(const limited_source&) = delete;
+
+    /**
+     * Move constructor
+     * 
+     * @param other other instance
+     */
+    limited_source(limited_source&& other) :
+    src(std::move(other.src)),
+    limit_bytes(other.limit_bytes) {
+        other.limit_bytes = 0;
+    }
+
+    /**
+     * Move assignment operator
+     * 
+     * @param other other instance
+     * @return this instance
+     */
+    limited_source& operator=(limited_source&& other) {
+        src = std::move(other.src);
+        limit_bytes = other.limit_bytes;
+        other.limit_bytes = 0;
+        return *this;
+    }
+
+    /**
+     * Counting read implementation
+     * 
+     * @param buffer output buffer
+     * @param length number of bytes to process
+     * @return number of bytes processed
+     */
+    std::streamsize read(char* buffer, std::streamsize length) {
+        namespace sc = staticlib::config;
+        if (!sc::is_sizet(length)) throw IOException(TRACEMSG(
+                "Invalid 'read' parameter specified, length: [" + sc::to_string(length) + "]"));
+        size_t ulen = static_cast<size_t> (length);
+        if (ulen > limit_bytes) throw IOException(TRACEMSG(
+                "Read limit threshold exceeded, " + 
+                " limit: [" + sc::to_string(limit_bytes) + "]," +
+                " requested length: [" + sc::to_string(length) + "]"));
+        if (src.get_count() > limit_bytes - ulen) throw IOException(TRACEMSG(
+                "Read limit threshold exceeded, " +
+                " already read: [" + sc::to_string(src.get_count()) + "]" +
+                " limit: [" + sc::to_string(limit_bytes) + "]," +
+                " requested length: [" + sc::to_string(length) + "]"));
+        return src.read(buffer, length);
+    }
+
+    /**
+     * Returns number of bytes read through this instance
+     * 
+     * @return number of bytes read through this instance
+     */
+    size_t get_count() {
+        return src.get_count();
+    }
+
+    /**
+     * Underlying source accessor
+     * 
+     * @return underlying source reference
+     */
+    Source& get_source() {
+        return src.get_source();
+    }
+
+};
+
+/**
+ * Factory function for creating limited sources,
+ * created source wrapper will own specified source
+ * 
+ * @param source input source
+ * @param limit_bytes max number of bytes allowed to be read from this source
+ * @return limited source
+ */
+template <typename Source,
+class = typename std::enable_if<!std::is_lvalue_reference<Source>::value>::type>
+limited_source<Source> make_limited_source(Source&& source, size_t limit_bytes) {
+    return limited_source<Source>(std::move(source), limit_bytes);
+}
+
+/**
+ * Factory function for creating limited sources,
+ * created source wrapper will NOT own specified source
+ * 
+ * @param source input source
+ * @param limit_bytes max number of bytes allowed to be read from this source
+ * @return limited source
+ */
+template <typename Source>
+limited_source<reference_source<Source>> make_limited_source(Source& source, size_t limit_bytes) {
+    return limited_source<reference_source<Source>> (make_reference_source(source), limit_bytes);
+}
+
+} // namespace
+}
+
+#endif	/* STATICLIB_IO_LIMITED_SOURCE_HPP */
+
---
+++ test/limited_source_test.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * File:   limited_source_test.cpp
+ * Author: alex
+ *
+ * Created on January 29, 2017, 4:42 PM
+ */
+
+#include "staticlib/io/limited_source.hpp"
+
+#include <array>
+#include <iostream>
+#include <string>
+
+#include "staticlib/config/assert.hpp"
+
+#include "TwoBytesAtOnceSource.hpp"
+#include "test_utils.hpp"
+
+namespace io = staticlib::io;
+
+void test_limit() {
+    auto src = io::make_limited_source(TwoBytesAtOnceSource{"bar1"}, 3);
+    std::array<char, 2> arr;
+    auto read = src.read(arr.data(), arr.size());
+    slassert(throws_exc([&src] { src.read(nullptr, 4); }));
+    slassert(2 == read);
+    slassert(2 == src.get_count());
+    slassert(throws_exc([&src] { src.read(nullptr, 2); }));
+    slassert(!throws_exc([&src, &arr] { src.read(arr.data(), 1); }));
+    slassert(throws_exc([&src, &arr] { src.read(arr.data(), 1); }));
+}
+
+int main() {
+    try {
+        test_limit();
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
