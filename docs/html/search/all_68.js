2ad39c576b7e992e61cca7c023d6f14f2bc256c1
alex <alex@staticlibs.net>
2018-10-17 23:10:15 +0100

    1.2.11 docs

--- docs/html/search/all_68.js
+++ docs/html/search/all_68.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['hex_5ffrom_5fstring',['hex_from_string',['../namespacestaticlib_1_1io.html#a260966ef378d6002d138aff9f1ddd688',1,'staticlib::io']]],
   ['hex_5foperations_2ehpp',['hex_operations.hpp',['../hex__operations_8hpp.html',1,'']]],
   ['hex_5fsink',['hex_sink',['../classstaticlib_1_1io_1_1hex__sink.html',1,'staticlib::io']]],
   ['hex_5fsink',['hex_sink',['../classstaticlib_1_1io_1_1hex__sink.html#a64f7218adf88bf7d857bcc6185bf6153',1,'staticlib::io::hex_sink::hex_sink(Sink &amp;&amp;sink)'],['../classstaticlib_1_1io_1_1hex__sink.html#aeea11d93f4c23fc3409572a050a9c1c5',1,'staticlib::io::hex_sink::hex_sink(const hex_sink &amp;)'],['../classstaticlib_1_1io_1_1hex__sink.html#a9dcb5ff4cc6d9189e5f4d90680b6e627',1,'staticlib::io::hex_sink::hex_sink(hex_sink &amp;&amp;other) STATICLIB_NOEXCEPT']]],
--- docs/html/search/functions_68.js
+++ docs/html/search/functions_68.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['hex_5ffrom_5fstring',['hex_from_string',['../namespacestaticlib_1_1io.html#a260966ef378d6002d138aff9f1ddd688',1,'staticlib::io']]],
   ['hex_5fsink',['hex_sink',['../classstaticlib_1_1io_1_1hex__sink.html#a64f7218adf88bf7d857bcc6185bf6153',1,'staticlib::io::hex_sink::hex_sink(Sink &amp;&amp;sink)'],['../classstaticlib_1_1io_1_1hex__sink.html#aeea11d93f4c23fc3409572a050a9c1c5',1,'staticlib::io::hex_sink::hex_sink(const hex_sink &amp;)'],['../classstaticlib_1_1io_1_1hex__sink.html#a9dcb5ff4cc6d9189e5f4d90680b6e627',1,'staticlib::io::hex_sink::hex_sink(hex_sink &amp;&amp;other) STATICLIB_NOEXCEPT']]],
   ['hex_5fsource',['hex_source',['../classstaticlib_1_1io_1_1hex__source.html#a5e4c2ebae4122f4217ba31b527189950',1,'staticlib::io::hex_source::hex_source(Source &amp;&amp;src)'],['../classstaticlib_1_1io_1_1hex__source.html#ab67270a83c53c71b9b96560a8f04a993',1,'staticlib::io::hex_source::hex_source(const hex_source &amp;)'],['../classstaticlib_1_1io_1_1hex__source.html#ad0f776c23baa5014246bda6090e771b7',1,'staticlib::io::hex_source::hex_source(hex_source &amp;&amp;other) STATICLIB_NOEXCEPT']]]
 ];
